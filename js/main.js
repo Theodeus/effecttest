f3f600d58dd034f7e2cc28391b2a58cbde66dc95
Oskar Eriksson <oskar.det@gmail.com>
2012-02-26 16:44:51 +0100

    Added some additional skeletal files and started on the soundfile module

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //main router, imports it's dependencies via require.js
 
-require(["soundfile", "effects", "play"], function(soundfile, effects, play){
-    
+require(["third-party/underscore-min", "third-party/backbone-min", "soundfile", "effects", "play"], function(_, backbone, soundfile, effects, play){
+    console.log(_, backbone, soundfile, effects, play);
     //main program flow
-    
+    console.log(soundfile.getSong());
+    soundfile.changeFile("sounds/song.mp3");
+    setTimeout(function(){console.log(soundfile.getSong())},1000);
     
 });
--- js/soundfile.js
+++ js/soundfile.js
@@ -1,1 +1,28 @@
 //soundfile abstraction
+
+define(function(){
+    
+    var xhr, onLoad, song = "hej", url = "sounds/song.mp3", title = "untitled";
+    
+    onLoad = function(){
+        if(xhr.readyState = 4 && (xhr.status === 200 || xhr.status === 304)){
+            song = xhr.response;
+        }
+    };
+    
+    return {        
+        changeFile : function(newUrl, title){           
+            url = newUrl;
+            title = title || "untitled";             
+            xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.responseType = "arraybuffer";
+            xhr.onreadystatechange = onLoad;
+            xhr.send(null);
+        },
+        getSong : function() {
+            return song;
+        }
+        
+    };
+});
