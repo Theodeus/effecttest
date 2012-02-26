f89508935e25e5f2cc22dedfb4bc97d411b39c83
Oskar Eriksson <oskar.det@gmail.com>
2012-02-26 18:13:39 +0100

    Started including backbone into the app. Stuck at the view for soundfile. Possible that this idea with the soundfilemodule is invalid somehow...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 //main router, imports it's dependencies via require.js
 
-require(["third-party/underscore-min", "third-party/backbone-min", "soundfile", "effects", "play"], function(_, backbone, soundfile, effects, play){
-    console.log(_, backbone, soundfile, effects, play);
+require(["third-party/underscore-min", "third-party/backbone-min", "soundfile", "effects", "play"], function(_, backbone, soundfile, effects, play) {
+
     //main program flow
-    console.log(soundfile.getSong());
-    soundfile.changeFile("sounds/song.mp3");
-    setTimeout(function(){console.log(soundfile.getSong())},1000);
-    
+    var song = new soundfile.model("sounds/song.mp3", "a short song");
+    console.dir(soundfile.view);
+    var songView = new soundfile.view({model: song});
+    console.log(songView);
+    setTimeout(function() {
+       console.log(song.get("song"), song.get("url"), song.get("title"));
+    }, 1000);
 });
--- js/soundfile.js
+++ js/soundfile.js
@@ -1,28 +1,47 @@
 //soundfile abstraction
 
-define(function(){
-    
-    var xhr, onLoad, song = "hej", url = "sounds/song.mp3", title = "untitled";
-    
-    onLoad = function(){
-        if(xhr.readyState = 4 && (xhr.status === 200 || xhr.status === 304)){
-            song = xhr.response;
-        }
+define(["third-party/underscore-min", "third-party/backbone-min"], function() {
+
+    var soundfile = {
+        model : Backbone.Model.extend({
+            initialize : function(_url, _title) {
+                _.bindAll(this, "changeFile");
+                this.set({
+                    url : _url,
+                    title : _title
+                });
+                this.changeFile(_url, _title);
+            },
+            changeFile : function(newUrl, title) {
+                this.set({
+                    url : newUrl
+                });
+                this.set({
+                    title : title || "untitled"
+                });
+                var that = this;
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", this.get("url"), true);
+                xhr.responseType = "arraybuffer";
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState = 4 && (xhr.status === 200 || xhr.status === 304)) {
+                        that.set({
+                            song : xhr.response
+                        });
+                    }
+                };
+                xhr.send(null);
+            }
+        }),
+        view : Backbone.View.extend({
+            initialize : function() {
+                _.bindAll(this, "render");
+            },
+            render : function() {
+                this.el.innerHTML = "I'm a song!";
+                return this;
+            }
+        })
     };
-    
-    return {        
-        changeFile : function(newUrl, title){           
-            url = newUrl;
-            title = title || "untitled";             
-            xhr = new XMLHttpRequest();
-            xhr.open("GET", url, true);
-            xhr.responseType = "arraybuffer";
-            xhr.onreadystatechange = onLoad;
-            xhr.send(null);
-        },
-        getSong : function() {
-            return song;
-        }
-        
-    };
+    return soundfile;
 });
