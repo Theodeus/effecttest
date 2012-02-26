e282d64b6a615f3dd4708e9297c40e8f2c2117d0
Oskar Eriksson <oskar.det@gmail.com>
2012-02-26 16:07:52 +0100

    Added project structure

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//main router, imports it's dependencies via require.js
+
+require(["soundfile", "effects", "play"], function(soundfile, effects, play){
+    
+    //main program flow
+    
+    
+});
